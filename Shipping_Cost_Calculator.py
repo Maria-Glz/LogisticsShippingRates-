6f8375e3bac8f91616cc15af17a19af914d6622a
Mar-Barr <118563943+Maria-Glz@users.noreply.github.com>
2025-12-26 16:13:04 -0600

    edit the .py file
    Added a thank you message after displaying the shipping cost.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -9,3 +9,6 @@ shipping_cost = weight * rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
+
+ # Here is a new update by <maria-glz>
+print("Thank you for using the Shipping Cost Calculator!")
